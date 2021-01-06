08bc3e4a65c21f9d7e0f986a96b49bfecd88091c
Azad <azadkaratas@gmail.com>
2021-01-06 22:51:10 +0300

    MPU9250 library files are added.
    MPU9250 library will be added. Files are added.

---
+++ firmware/Core/Inc/mpu9250.h
@@ -0,0 +1,14 @@
+/**
+ ******************************************************************************
+ * @file           mpu9250.h
+ * @brief          Contains MPU9250 IMU driver and related functions
+ * @author 		Azad Karataş
+ ******************************************************************************
+ */
+
+#ifndef INC_MPU9250_H_
+#define INC_MPU9250_H_
+
+
+
+#endif /* INC_MPU9250_H_ */
